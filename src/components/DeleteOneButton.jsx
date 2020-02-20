528446bd76ab3817ad5a2fddc03f916e23783a1d
dabgan <gabriel.daniluk.work@gmail.com>
2020-02-20 20:28:08 +0100

    docs: add readme, change app title

--- src/components/DeleteOneButton.jsx
+++ src/components/DeleteOneButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DeleteOneButton({ deleteOne, isAvailable, loaderIcon }) {
     const buttonStyle = isAvailable ? "" : "btn-disabled";
-    const isIconVisible = loaderIcon ? "none" : "inline-block";
+    const isIconVisible = loaderIcon ? "none" : "inline-block ";
 
     return (
         <div className="prompt">
