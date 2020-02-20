120031890287d79b90f899575ac91ce931eb0bb5
dabgan <gabriel.daniluk.work@gmail.com>
2020-02-20 17:48:13 +0100

    Updates

---
+++ precache-manifest.ce7b92e84092b18d6249901a05d634c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41933c35e09c874aed8ff47fe99453f",
+    "url": "/Project06-React_Quiz/index.html"
+  },
+  {
+    "revision": "1a9d4fc5aa21bdff5022",
+    "url": "/Project06-React_Quiz/static/css/main.f6ce0a46.chunk.css"
+  },
+  {
+    "revision": "98fa70d7868f7a0bf1a2",
+    "url": "/Project06-React_Quiz/static/js/2.bb9a253b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Project06-React_Quiz/static/js/2.bb9a253b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9d4fc5aa21bdff5022",
+    "url": "/Project06-React_Quiz/static/js/main.edcd6a1a.chunk.js"
+  },
+  {
+    "revision": "a436b8fcb804673fed68",
+    "url": "/Project06-React_Quiz/static/js/runtime-main.fc743b52.js"
+  }
+]);
