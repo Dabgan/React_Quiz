dea2dbf4f7576b26d199d3146e56cda27a0cb51d
dabgan <gabriel.daniluk.work@gmail.com>
2020-11-23 12:26:24 +0100

    fix: bug with flashing 'next question' button

--- src/components/Question/QuestionPanel.jsx
+++ src/components/Question/QuestionPanel.jsx
@@ -14,33 +14,25 @@ function QuestionPanel({
     const isIconVisible = loaderIcon ? "none" : "flex";
     const isAnswerSubmitted = isAnswerChosen ? "warning error" : "warning";
     return (
-        <div className="question-panel">
+        <div className="question-panel" style={{ display: isIconVisible }}>
             <div className="quiz-information">
                 <HintButton
-                    loaderIcon={loaderIcon}
                     deleteMethod={fiftyFifty}
                     isAvailable={isAvailableFiftyFifty}
                     btnText="50:50"
                 />
 
-                <span
-                    style={{ display: `${isIconVisible}` }}
-                    className=" question-counter"
-                >
+                <span className=" question-counter">
                     <span>{currentQuestion}/10</span>
                 </span>
 
                 <HintButton
-                    loaderIcon={loaderIcon}
                     deleteMethod={deleteOne}
                     isAvailable={isAvailableDeleteOne}
                     btnText="Delete One"
                 />
             </div>
-            <div
-                style={{ display: `${isIconVisible}` }}
-                className="question-container"
-            >
+            <div className="question-container">
                 <h1 className="question-heading">{question.quest}</h1>
             </div>
             <p className={isAnswerSubmitted}>Please select an answer!</p>
